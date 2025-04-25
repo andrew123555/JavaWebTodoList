7a834c251db7214b5aa5ad95563c8853019217ec
andrew123555 <bb81245g000@gmail.com>
2025-04-25 11:41:02 +0800

    0425

--- src/main/java/dao/TodoListDao.java
+++ src/main/java/dao/TodoListDao.java
@@ -1,5 +1,24 @@
 package dao;
 
-public class TodoListDao {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import model.entity.Todo;
+
+public interface TodoListDao {
+	public List<Todo> todo = new CopyOnWriteArrayList<>();
+	
+	
+	List<Todo> findAll();
+	
+	Todo getTodo(Integer id);
+	
+	void addTodo (Todo todo);
+	
+	void updateTodoComplete(Integer id , Boolean completed);
+	
+	void updateTodoText(Integer id , String text);
+	
+	void deleteTodo(Integer id );
+	
 }
